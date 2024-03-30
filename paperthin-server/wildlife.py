3094a2f7608b59b4c35a2cbd102b77a544038b75
LionsPhil <LionsPhil@users.noreply.github.com>
2024-03-30 23:34:50 +0000

    Use srcsets to improve image resolution
    Now fetching nicer high-res images that are getting *down*scaled rather
    than *up*scaled.

--- paperthin-server/wildlife.py
+++ paperthin-server/wildlife.py
@@ -13,6 +13,9 @@ from PIL import Image
 _BASE_PAGE = 'https://www.theguardian.com/environment/series/weekinwildlife'
 _WEEK_REGEX = re.compile(
     r'href="(https://www.theguardian.com/environment/gallery/[^"]+)"')
+_PICTURE_START_REGEX = re.compile(r'<picture>')
+_PICTURE_END_REGEX = re.compile(r'</picture>')
+_SRCSET_REGEX = re.compile(r'srcset="([^"]+)"')
 _IMG_START_REGEX = re.compile(r'<img class="gallery__img')
 _ALT_REGEX = re.compile(r'alt="([^"]+)"')
 _SRC_REGEX = re.compile(r'src="([^"]+)"')
@@ -51,8 +54,21 @@ def find_pictures_of_week(session: requests.Session, url: str|None
     line: str
     alt: str
     src: str
+    srcset: str = ''
     in_image = False
     for line in response.iter_lines(decode_unicode=True):
+        # Look for a <picture>, and remember the first srcset attribute we see
+        # in it. Forget again upon seeing another, or leaving it.
+        if _PICTURE_START_REGEX.search(line) or _PICTURE_END_REGEX.search(line):
+            srcset = ''
+        if not srcset:
+            maybe_srcset = _SRCSET_REGEX.search(line)
+            if maybe_srcset:
+                # srcset is an evil attribute with extra stuff packed into it,
+                # whitespace separated. Take only the first token.
+                srcset = maybe_srcset.group(1).split(' ', 1)[0]
+
+        # Look for an <img>.
         if _IMG_START_REGEX.search(line):
             in_image = True
             alt = ''
@@ -64,10 +80,15 @@ def find_pictures_of_week(session: requests.Session, url: str|None
             maybe_src = _SRC_REGEX.search(line)
             if maybe_src:
                 src = maybe_src.group(1)
-                # Tiny bad HTML entity decoding.
-                src = src.replace('&amp;', '&')
             if _END_REGEX.search(line):
+                # If we had found a srcset, use that instead. The first one
+                # (and we ignore any subsequent ones) *should* be the best.
+                if srcset:
+                    src = srcset
+                # If we had found a source, remember the found image.
                 if src:
+                    # Tiny bad HTML entity decoding.
+                    src = src.replace('&amp;', '&')
                     urls.append([src, alt])
                 in_image = False
 
